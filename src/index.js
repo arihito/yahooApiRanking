00075d9adaab2a283dc8763f09fc7852f7f90271
arihito <arihito.m@gmail.com>
2018-07-02 16:54:55 +0900

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+// reducreを結合
+combineReducers(reducers),
+// ミドルウェアの読み込み
+applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
